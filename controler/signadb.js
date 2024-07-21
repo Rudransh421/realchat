a6793fe8235f54cb7e2e9f6048ee2f03f469e35f
Rudransh421 <rudransh7381@gmail.com>
2024-07-21 19:13:49 +0530

    Add .gitattributes to normalize line endings

--- controler/signadb.js
+++ controler/signadb.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/realchat')
+ mongoose.connect('mongodb://localhost:27017/realchat')
 .then(()=>{
     console.log("mongodb connected");
 })
 .catch((err)=>{
     console.log("not connected mongodb ");
 })
+
+
+
 
 const loginschema = new mongoose.Schema({
     name:{
